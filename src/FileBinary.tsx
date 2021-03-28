794095c155e2953e94477d87abaa5a6b11d5fa03
Eduardo Morales <edumorales@google.com>
2021-03-29 00:31:58 +0200

    minor

---
+++ src/FileBinary.tsx
@@ -0,0 +1,137 @@
+import fileDownload from 'js-file-download';
+import {useState} from 'preact/hooks';
+
+export default function FileBinary(props: {
+  file: File;
+  status: string;
+}): JSX.Element {
+  const [fileDownloaded, setFileDownloaded] = useState<boolean>(false);
+  const fileAbbreviation = props.file?.name.split('.').pop() || '';
+
+  const readFileContent = (file: File) => {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = event => resolve(event?.target?.result);
+      reader.onerror = error => reject(error);
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const corruptAndDownload = (): void => {
+    setFileDownloaded(true);
+    readFileContent(props.file).then(buffer => {
+      const binary = new Int8Array(buffer as ArrayBuffer);
+      const corruptedBinary = binary.map(elem => elem + 1);
+      fileDownload(corruptedBinary, props.file.name);
+    });
+  };
+
+  return (
+    <div className={'file'} onClick={corruptAndDownload}>
+      <FileIconSVG fileAbbreviation={fileAbbreviation} />
+      <a>{props.file?.name}</a>
+      {props.status === 'download' && (
+        <DownloadSVG downloaded={fileDownloaded} />
+      )}
+    </div>
+  );
+}
+
+const stringToColor = (str: string): string => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ('00' + value.toString(16)).substr(-2);
+  }
+  return color;
+};
+
+const FileIconSVG = (props: {fileAbbreviation: string}): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      id="body_1"
+      width="72"
+      height="72"
+    >
+      <g transform="matrix(0.140625 0 0 0.140625 0 0)">
+        <g transform="matrix(1 0 0 1 0 0)"></g>
+        <path
+          transform="matrix(1 0 0 1 0 0)"
+          d="M128 0C 110.4 0 96 14.4 96 32L96 32L96 480C 96 497.616 110.4 512 128 512L128 512L448 512C 465.6 512 480 497.616 480 480L480 480L480 128L352 0L128 0z"
+          stroke="none"
+          fill="#E2E5E7"
+          fill-rule="nonzero"
+        />
+        <g transform="matrix(1 0 0 1 0 0)"></g>
+        <path
+          transform="matrix(1 0 0 1 0 0)"
+          d="M384 128L480 128L352 0L352 96C 352 113.6 366.4 128 384 128z"
+          stroke="none"
+          fill="#B0B7BD"
+          fill-rule="nonzero"
+        />
+        <g transform="matrix(1 0 0 1 0 0)"></g>
+        <path
+          transform="matrix(1 0 0 1 0 0)"
+          d="M480 224L384 128L480 128L480 224"
+          stroke="none"
+          fill="#CAD1D8"
+          fill-rule="nonzero"
+        />
+        <g transform="matrix(1 0 0 1 0 0)"></g>
+        <path
+          transform="matrix(1 0 0 1 0 0)"
+          d="M417 416C 417 424.8 409.8 432 401 432L401 432L49 432C 40.2 432 33 424.8 33 416L33 416L33 256C 33 247.2 40.2 240 49 240L49 240L401 240C 409.8 240 417 247.2 417 256L417 256L417 416z"
+          stroke="none"
+          fill={stringToColor(props.fileAbbreviation)}
+          fill-rule="nonzero"
+        />
+        <g transform="matrix(1 0 0 1 0 0)"></g>
+        <path
+          transform="matrix(1 0 0 1 0 0)"
+          d="M400 432L96 432L96 448L400 448C 408.8 448 416 440.8 416 432L416 432L416 416C 416 424.8 408.8 432 400 432z"
+          stroke="none"
+          fill="#CAD1D8"
+          fill-rule="nonzero"
+        />
+        <text
+          transform="matrix(1 0 0 1 0 0)"
+          x="63.5"
+          y="371"
+          style="fill:#FFFFFF;font-family:'Montserrat',sans-serif;"
+          font-size="96"
+        >
+          {props.fileAbbreviation.toUpperCase()}
+        </text>
+      </g>
+    </svg>
+  );
+};
+
+function DownloadSVG(props: {downloaded: boolean}): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 100 100"
+      id="Layer_1"
+      version="1.1"
+      width="40"
+      className="download-button"
+    >
+      <g>
+        <path
+          d="M9.843 52.312L50 92.469l40.157-40.157-8.032-8.031-26.446 26.446V7.469H44.321v63.258L17.875 44.281z"
+          fill={props.downloaded ? 'grey' : '#00539C'}
+          id="XMLID_1312_"
+        ></path>
+      </g>
+    </svg>
+  );
+}
---
+++ src/app.tsx
@@ -0,0 +1,67 @@
+import {useState} from 'preact/hooks';
+import FileBinary from './FileBinary';
+import ShrederSVG from './ShrederSVG';
+
+export function App() {
+  const [files, setFiles] = useState<File[]>([]);
+  const [status, setStatus] = useState<string>('initial');
+
+  const drop = (event: any) => {
+    event.preventDefault();
+    const fileAsDataTransfer = Object.values(
+      event.dataTransfer.items
+    ) as DataTransferItem[];
+
+    if (fileAsDataTransfer) {
+      const fileObjs: File[] = fileAsDataTransfer.map(
+        file => file.getAsFile() as File
+      );
+
+      setFiles(fileObjs);
+      setStatus('ready');
+    }
+  };
+
+  const drag = (event: any) => {
+    event.preventDefault();
+  };
+
+  const corrupt = () => {
+    setStatus('corrupt');
+    setTimeout(() => {
+      setStatus('download');
+    }, 3000);
+  };
+
+  return (
+    <>
+      <div className="title">
+        <h1>Online File Corruptor</h1>
+        <h5>Corrupt it, send it, and blame it on their computer</h5>
+      </div>
+      <div id="drop_zone" onDrop={drop} onDragOver={drag}>
+        <DropZone status={status} files={files} />
+      </div>
+
+      {status === 'ready' && (
+        <button class={'corrupt-btn'} onClick={corrupt}>
+          Corrupt All Uploaded Files
+        </button>
+      )}
+    </>
+  );
+}
+
+const DropZone = (props: {status: string; files: File[]}) => {
+  if (props.status === 'corrupt') return <ShrederSVG />;
+  if (['ready', 'download'].includes(props.status)) {
+    return (
+      <>
+        {props.files.map(file => (
+          <FileBinary file={file} status={props.status} />
+        ))}
+      </>
+    );
+  }
+  return <h3>Drag and drop multiple files</h3>;
+};
